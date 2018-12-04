a566072d5694353d80f54e05a59a1e388dc28d2e
RaghavAwasthi <raghavawasthi2014@gmail.com>
2018-12-04 13:27:52 +0530

    Added Image Picking feature

--- app/src/main/java/com/community/jboss/visitingcard/VisitingCard/VisitingCardActivity.java
+++ app/src/main/java/com/community/jboss/visitingcard/VisitingCard/VisitingCardActivity.java
@@ -1,21 +1,43 @@
 package com.community.jboss.visitingcard.VisitingCard;
 
+import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 
-import com.community.jboss.visitingcard.LoginActivity;
+import com.bumptech.glide.Glide;
 import com.community.jboss.visitingcard.Maps.MapsActivity;
 import com.community.jboss.visitingcard.R;
 import com.community.jboss.visitingcard.SettingsActivity;
+import com.mikhaellopez.circularimageview.CircularImageView;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class VisitingCardActivity extends AppCompatActivity {
+    int SELECT_PICTURE = 1254;
+    int REQUEST_PERMISSION = 1425;
+    int REQUEST_IMAGE = 2586;
+    private static String imageFilePath = "";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,7 +66,34 @@ public class VisitingCardActivity extends AppCompatActivity {
                         }).show();
             }
         });
-
+        if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) !=
+                PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    REQUEST_PERMISSION);
+        }
+        CircularImageView user_image = findViewById(R.id.user_image);
+        user_image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder selector = new AlertDialog.Builder(VisitingCardActivity.this);
+                selector.setTitle("Select from");
+                String[] Items = {"Gallery", "Camera"};
+                selector.setItems(Items, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int pos) {
+                        switch (pos) {
+                            case 0:
+                                pickImage();
+                                break;
+                            case 1:
+                                captureImage();
+                                break;
+                        }
+                    }
+                });
+                selector.show();
+            }
+        });
     }
 
     @Override
@@ -63,6 +112,59 @@ public class VisitingCardActivity extends AppCompatActivity {
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void pickImage() {
+
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), SELECT_PICTURE);
+
+    }
+
+    private void captureImage() {
+        Intent pictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (pictureIntent.resolveActivity(getPackageManager()) != null) {
+
+            File photoFile = null;
+            try {
+                String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(new Date());
+                String imageFileName = "IMG_" + timeStamp + "_";
+                File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+                photoFile = File.createTempFile(imageFileName, ".jpg", storageDir);
+                imageFilePath = photoFile.getAbsolutePath();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                return;
+            }
+            Uri photoUri = FileProvider.getUriForFile(this, getPackageName() + ".provider", photoFile);
+            pictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+            startActivityForResult(pictureIntent, REQUEST_IMAGE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            if (requestCode == SELECT_PICTURE) {
+
+                //Get ImageURi and load with help of picasso
+                //Uri selectedImageURI = data.getData();
+
+                Glide.with(getApplicationContext())
+                        .load(data.getData())
+                        .into((CircularImageView) findViewById(R.id.user_image));
+            }
+
+        }
+        if (requestCode == REQUEST_IMAGE) {
+            if (resultCode == RESULT_OK) {
+                ((CircularImageView) findViewById(R.id.user_image)).setImageURI(Uri.parse(imageFilePath));
+            }
         }
     }
 }
